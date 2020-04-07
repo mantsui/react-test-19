b02230bee8b5a2b4347411abb5c33120400d90c0
Man H Tsui <mantsui@ucdavis.edu>
2020-04-07 13:44:03 -0700

    Update ConfigDialog.js

--- src/ConfigDialog.js
+++ src/ConfigDialog.js
@@ -16,7 +16,7 @@ class ConfigDialog extends React.Component {
         //this.setState({ selectedTabIndex: 0 });
 
         try{
-            window.tableau.extensions.initializeDialogAsync().then(() => {
+            /*window.tableau.extensions.initializeDialogAsync().then(() => {
                 console.log('Debug: component will mount.');
                 const tableauSettings = tableau.extensions.settings.getAll();
                 const tableauDashboard = tableau.extensions.dashboardContent.dashboard;
@@ -50,7 +50,7 @@ class ConfigDialog extends React.Component {
         }
         catch(err){
             console.log('window.tableau.extensions did not load');
-        }
+        }*/
     }
 
     render() {
