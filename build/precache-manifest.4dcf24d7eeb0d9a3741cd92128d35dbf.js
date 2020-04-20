0c00af3af77e4bf0848711ad620e03ec66d4d857
Man H Tsui <mantsui@ucdavis.edu>
2020-04-20 12:00:28 -0700

    update gitignore

---
+++ build/precache-manifest.4dcf24d7eeb0d9a3741cd92128d35dbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835bbc61430f58354da92229de0c0849",
+    "url": "/react-test-19/index.html"
+  },
+  {
+    "revision": "e822016e178cc920a222",
+    "url": "/react-test-19/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "725359be6993965e8d53",
+    "url": "/react-test-19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e822016e178cc920a222",
+    "url": "/react-test-19/static/js/2.7c9de79b.chunk.js"
+  },
+  {
+    "revision": "e4e073bc5921c6ef2b9014a9201097f3",
+    "url": "/react-test-19/static/js/2.7c9de79b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725359be6993965e8d53",
+    "url": "/react-test-19/static/js/main.af6aef1d.chunk.js"
+  },
+  {
+    "revision": "ff242f08d8ac04b012c7",
+    "url": "/react-test-19/static/js/runtime-main.0eefecec.js"
+  }
+]);
