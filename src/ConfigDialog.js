f01ffaa0c927fa3e70fcdf2d08812d6e235fa794
Man H Tsui <mantsui@ucdavis.edu>
2020-04-19 22:57:01 -0700

    Update ConfigDialog.js

--- src/ConfigDialog.js
+++ src/ConfigDialog.js
@@ -1,6 +1,6 @@
 //import './tableau-extensions-1.latest.js';
 import 'react-tabs/style/react-tabs.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LineDropdown from './LineDropdown.js'
 import RadioButton from './RadioButton.js'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
@@ -9,95 +9,69 @@ let radioList = ['one', 'two', 'three'];
 //<RadioButton radioData={['one', 'two', 'three','four']} radioTitle='This is Radio Titlez'/>
 
 
+function ConfigDialog () {
 
-class ConfigDialog extends React.Component {
+    const [selectedSheet, setSelectedSheet] = useState(undefined);
+    const [sheetNames, setSheetNames] = useState([]);
 
-    componentWillMount() {
-        //this.setState({ selectedTabIndex: 0 });
+    useEffect(() => {
+        console.log('useFffect running');
 
-        try{
-            window.tableau.extensions.initializeDialogAsync().then(() => {
-                console.log('Debug: component will mount.');
-                const tableauSettings = tableau.extensions.settings.getAll();
-                const tableauDashboard = tableau.extensions.dashboardContent.dashboard;
+        tableau.extensions.initializeAsync().then(() => {
+            const tabSelectedSheet = tableau.extensions.settings.get('sheet');
+            setSelectedSheet(tabSelectedSheet);
 
-                tableauDashboard.worksheets.forEach(function (worksheet) {
-                    sheetList02.push(worksheet.name);
+            const tabSheetNames = tableau.extensions.dashboardContent.dashboard.worksheets.map(worksheet => worksheet.name);
+            setSheetNames(sheetNames);
+        });
 
-                    worksheet.getSummaryDataAsync({ maxRows: 1 }).then(function (sumdata) {
-                        console.log('Debug running worksheet.getSummaryDataAsync... ');
+        console.log('Console log output for tableau sheet');
+        console.log(selectedSheet);
+    }, []);
 
-                        sumdata.columns.forEach(function (current_value) {
+    return(
+        <>
+        <h1>Tableau Extension D3 Sankey Diagram ConfigDialog.</h1>
 
-                            if(current_value.dataType === 'float' || current_value.dataType === 'int') {
-                                measureColumns.push(current_value.fieldName);
-                            }
-                    
-                            if(current_value.dataType === 'string') {
-                                dimensionColumns.push(current_value.fieldName);
-                            }
-                    
-                        });
-                    });
-                });
+        <Tabs>
+            <TabList>
+                <Tab>Main</Tab>
+                <Tab>Optional: Filter Actions</Tab>
+                <Tab>Optional: Formatting</Tab>
+                <Tab>About</Tab>
+            </TabList>
+            
+            <TabPanel>
+                <p>Main, core options</p>
+                <br/>
+                <LineDropdown />
+            </TabPanel>
 
-                console.log('Debug tableauSettings: ', tableauSettings);
-                console.log('Debug sheetList02: ', sheetList02);
-                console.log('Debug tableauDashboard: ', tableauDashboard);
-                console.log('Debug measureColumns: ', measureColumns);
-                console.log('Debug dimensionColumns: ', dimensionColumns);
-            });
-        }
-        catch(err){
-            console.log('window.tableau.extensions did not load');
-        }
-    }
+            <TabPanel>
+                <p>Optional features</p>
+                <br/>
+                <RadioButton radioData={radioList} radioTitle='This is Radio Titlez'/>
+                <br/>
+            </TabPanel>
 
-    render() {
+            <TabPanel>
+                <p>Optional formatting stuff</p>
+            </TabPanel>
 
-
-        return(
-            <>
-            <h1>Tableau Extension D3 Sankey Diagram ConfigDialog.</h1>
-
-            <Tabs>
-                <TabList>
-                    <Tab>Main</Tab>
-                    <Tab>Optional: Filter Actions</Tab>
-                    <Tab>Optional: Formatting</Tab>
-                    <Tab>About</Tab>
-                </TabList>
-                
-                <TabPanel>
-                    <p>Main, core options</p>
-                    <br/>
-                    <LineDropdown />
-                </TabPanel>
-
-                <TabPanel>
-                    <p>Optional features</p>
-                    <br/>
-                    <RadioButton radioData={radioList} radioTitle='This is Radio Titlez'/>
-                    <br/>
-                </TabPanel>
-
-                <TabPanel>
-                    <p>Optional formatting stuff</p>
-                </TabPanel>
-
-                <TabPanel>
-                    <p>About</p>
-                </TabPanel>
+            <TabPanel>
+                <p>About</p>
+            </TabPanel>
 
 
 
-            </Tabs>
+        </Tabs>
 
 
 
-            </>
-        )
-    }
+        </>
+    )
+
+
 }
 
 export default ConfigDialog
